a8450b393fe021a9c89de4073ccdf746c5acfe19
Aditya Gupta <adityag1@andrew.cmu.edu>
2015-03-05 20:56:09 -0500

    directed graph cycle

--- graph/graph.h
+++ graph/graph.h
@@ -15,10 +15,13 @@ class Graph
   int V;              // No. of vertices
   list <int> *adj;    // Pointer to an array containing adjacency lists
   bool isCyclicUtil(int v, bool visited[], int parent);
+  bool isCyclicUtil_directed(int v, bool visited[], bool rec_stack[]);
 public:
   Graph(int V);                 // Constructor
   void addEdge(int v, int w);   // to add an edge to graph
+  void addEdge_directed(int v, int w);   // to add an edge to graph
   bool isCyclic();              // returns true if there is a cycle
+  bool isCyclic_directed();              // returns true if there is a cycle
 };
 
 Graph::Graph(int V)
@@ -26,6 +29,12 @@ Graph::Graph(int V)
     this->V = V;
     adj = new list<int>[V];
 }
+
+void Graph::addEdge_directed(int v, int w)
+{
+    adj[v].push_back(w);
+}
+
 
 void Graph::addEdge(int v, int w)
 {
@@ -37,26 +46,29 @@ void Graph::addEdge(int v, int w)
 
 bool Graph::isCyclic(void)
 {
-  bool* is_visited = new bool[V];
+    bool* is_visited = new bool[V];
 
-  // initialise the array
-  for(int i = 0; i < V; i++)
-  {
-    is_visited[i] = false;
-  }
-
-  for(int j = 0; j < V; j++)
-  {
-    if(is_visited[j] == false)
+    // initialise the array
+    for(int i = 0; i < V; i++)
     {
-      if(isCyclicUtil(j, is_visited, -1))
-      {
-        return true;
-      }
+        is_visited[i] = false;
     }
-  }
-  return false;
+
+    for(int j = 0; j < V; j++)
+    {
+        if(is_visited[j] == false)
+        {
+            if(isCyclicUtil(j, is_visited, -1))
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
+
+
 
 
 bool Graph::isCyclicUtil(int v, bool visited[], int parent)
@@ -89,6 +101,58 @@ bool Graph::isCyclicUtil(int v, bool visited[], int parent)
   cout << "returning FALSE\t";
   cout << "v = " << v << "\tparent = "<< parent << endl;
   return false;
+}
+
+
+
+
+bool Graph::isCyclic_directed(void)
+{
+  bool* is_visited = new bool[V];
+  bool* rec_stack = new bool[V];
+
+  // initialise the array
+  for(int i = 0; i < V; i++)
+  {
+    is_visited[i] = false;
+    rec_stack[i] = false;
+  }
+
+  for(int j = 0; j < V; j++)
+  {
+      if(isCyclicUtil_directed(j, is_visited, rec_stack))
+      {
+        return true;
+      }
+  }
+  return false;
+}
+
+
+bool Graph::isCyclicUtil_directed(int v, bool visited[], bool rec_stack[])
+{
+    if(visited[v] == false)
+    {
+        visited[v] = true;
+        rec_stack[v] = true;
+
+        list<int>::iterator i;
+
+        for(i = adj[v].begin(); i != adj[v].end(); i++)
+        {
+            if(!visited[*i] && isCyclicUtil_directed(*i, visited, rec_stack))
+            {
+                return true;
+            }
+            else if(rec_stack[*i])
+            {
+                return true;
+            }
+        }
+    }
+
+    rec_stack[v] = false;
+    return false;
 }
 
 
--- graph/main.cpp
+++ graph/main.cpp
@@ -11,15 +11,23 @@ int main(int argc, char const *argv[])
   Graph g(5);
   cout<< sizeof(g) << endl;
 
-  g.addEdge(1, 0);
-  g.addEdge(0, 2);
-  g.addEdge(2, 0);
-  g.addEdge(0, 3);
-  g.addEdge(3, 4);
+  // g.addEdge(1, 0);
+  // g.addEdge(0, 2);
+  // g.addEdge(2, 0);
+  // g.addEdge(0, 3);
+  // g.addEdge(3, 4);
+
+  g.addEdge_directed(0, 1);
+  g.addEdge_directed(0, 2);
+  g.addEdge_directed(1, 2);
+  g.addEdge_directed(2, 0);
+  g.addEdge_directed(2, 3);
+  g.addEdge_directed(3, 3);
+
 
   cout<< sizeof(g) << endl;
 
-  g.isCyclic()? cout << "Graph contains cycle\n":
+  g.isCyclic_directed()? cout << "Graph contains cycle\n":
                      cout << "Graph doesn't contain cycle\n";
 
 
